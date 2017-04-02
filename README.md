ca9bfa58bdc4d8fcd9f092bef21bd932e8be7962
David <david@atraura.com>
2017-04-02 17:31:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Teleminder is a telegram bot to remind you to take your meds.
+
+Once a day, it sends you a message remembering to take your med at one specific time.
+
+    - If you take it, you will receive a random cat gif.
+
+    - If you take it late, it will send you a sad cat.
+
+    - If you don't take it, it will remind you again in half hour.
+
+
---
+++ bot.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import threading
+from random import randint
+
+import telebot
+from telebot import types
+import schedule
+import cat
+
+
+
+TOKEN = 'TOKEN'
+CHAT_ID = 123456
+IMAGES_PATH = os.path.dirname(os.path.abspath(__file__)) +'/images/'
+
+tb = telebot.TeleBot(TOKEN)
+
+def send_random_cat():
+    cat.getCat(directory= IMAGES_PATH + 'last_random_cat', filename='cat', format='gif')
+    photo = open(IMAGES_PATH + 'last_random_cat/cat.gif', 'rb')
+    tb.send_document(CHAT_ID, photo)
+
+
+def send_sad_cat():
+    photo = open(IMAGES_PATH +'/sad_cat/sad_cat'+str(randint(1,11))+'.jpg', 'rb')
+    tb.send_photo(CHAT_ID, photo)
+
+
+def send_reminder(message):
+    tb.reply_to(message, "Muy mal, te aviso en 30 min")
+    threading.Timer(60*30, send_message).start()
+
+def send_message():
+    markup = types.ReplyKeyboardMarkup()
+    markup.add('Sí, a la hora', 'Sí, tarde', 'Nop', 'Free Day' )
+    tb.send_message(CHAT_ID, "¿Te has tomado la pastilla?", None, None, markup)
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@tb.message_handler(func=lambda message: True)
+def process_message(message):
+    if message.text == u'Sí, a la hora':
+        send_random_cat()
+    elif message.text == u'Sí, tarde':
+        send_sad_cat()
+    elif message.text == u'Free Day':
+        send_random_cat()
+    elif message.text == u'Nop':
+        send_reminder(message)
+
+
+def main():
+    tb.send_message(CHAT_ID, "¡Hola! Soy PastiBot.", None, None, None)
+    schedule.every().day.at('9:30').do(send_message)
+
+    tb.polling()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
